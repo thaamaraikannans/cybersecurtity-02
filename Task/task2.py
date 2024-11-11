2b4572e1644ff49be2585c99f6848171872feafe
Thaamarai Kannan <thaamaraikannans.xbi4@gmail.com>
2024-11-11 19:23:13 +0530

    till task demo

--- Task/task2.py
+++ Task/task2.py
@@ -1,12 +1,8 @@
-
 str1=input("Enter the input :")
 print(list(str1))
 str2 = []
 for char in str1:
     str2.insert(0, char)
-str2 = "".join(str2)
-
-str1 = "".join(str1)
 str2 = "".join(str2)
 
 print(str1 + " and " + str2)
--- Task/test.py
+++ Task/test.py
@@ -1,8 +1,12 @@
-value = "charger"
+# value = "charger"
 
 
-print(value[:5])
-print(value[::2])
-print(value[:4:-1])
-print(value[::-2])
-print(value[::-1])
+# print(value[:5])
+# print(value[::2])
+# print(value[:4:-1])
+# print(value[::-2])
+# print(value[::-1])
+
+value = "".join(["k", "a", "n"])
+
+print(value)
